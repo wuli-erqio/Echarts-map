167d069c50f65dee416d5992dcea94d13230df5e
wuli-erqio <1766458200@qq.com>
2020-07-03 13:28:29 +0800

    定制柱状图bar

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@
   var myChart = echarts.init(document.querySelector(".bar .chart"));
   //设置配置项
   var option = {
-    color: ['#3398DB'],
+    color: ['#2f89cf'],
     tooltip: {
         trigger: 'axis',
         axisPointer: {            // 坐标轴指示器，坐标轴触发有效
@@ -12,31 +12,63 @@
         }
     },
     grid: {
-        left: '3%',
-        right: '4%',
-        bottom: '3%',
+        left: '0%',
+        top: '10px',
+        right: '0%',
+        bottom: '4%',
         containLabel: true
     },
     xAxis: [
         {
             type: 'category',
-            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+            data: ['旅游行业', '教育培训', '游戏行业', '医疗行业', '电商行业', '社交行业', '金融行业'],
             axisTick: {
                 alignWithLabel: true
+            },
+            // 修改刻度标签
+            axisLabel: {
+              color: "rgba(255, 255, 255, .6)",
+              fontSize: "12"
+            },
+            // x轴样式不显示
+            axisLine: {
+              show: false
             }
         }
     ],
     yAxis: [
         {
-            type: 'value'
+            type: 'value',
+            // 修改刻度标签
+            axisLabel: {
+              color: "rgba(255, 255, 255, .6)",
+              fontSize: 12
+            },
+            // y轴样式不显示
+            axisLine: {
+              lineStyle: {
+                color: "rgba(255, 255, 255, .1)",
+                width: 2
+              }
+            },
+            // y轴分割线的颜色
+            splitLine: {
+              lineStyle: {
+                color: "rgba(255, 255, 255, .1)"
+              }
+            }
         }
     ],
     series: [
         {
             name: '直接访问',
             type: 'bar',
-            barWidth: '60%',
-            data: [10, 52, 200, 334, 390, 330, 220]
+            barWidth: '35%',
+            data: [200, 300, 300, 900, 1500, 1200, 600],
+            itemStyle: {
+              // 设置柱状图圆角
+              barBorderRadius: 5
+            }
         }
     ]
   };
