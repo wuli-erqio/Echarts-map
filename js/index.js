e7dde74bcd72662336a07adfda82bce3f25a7681
wuli-erqio <1766458200@qq.com>
2020-07-03 14:50:43 +0800

    折线图line1完成

--- js/index.js
+++ js/index.js
@@ -179,3 +179,139 @@
     myChart.resize()
   })
 })();
+// 折线图1
+(function() {
+  var yearData = [
+    {
+      year: '2020',  // 年份
+      data: [  // 两个数组是因为有两条线
+          [24, 40, 101, 134, 90, 230, 210, 230, 120, 230, 210, 120],
+          [40, 64, 191, 324, 290, 330, 310, 213, 180, 200, 180, 79]
+        ]
+    },
+    {
+      year: '2021',  // 年份
+      data: [  // 两个数组是因为有两条线
+          [123, 175, 112, 197, 121, 67, 98, 21, 43, 64, 76, 38],
+          [143, 131, 165, 123, 178, 21, 82, 64, 43, 60, 19, 34]
+        ]
+      }
+    ];
+  var myChart = echarts.init(document.querySelector(".line1 .chart"));
+  option = {
+    color: ["#00f2f1", "#ed3f35"],
+    tooltip: {
+        trigger: 'axis'
+    },
+    legend: {
+        textStyle: {
+          color: "#4c9bfd"
+        },
+        right: "10%"
+    },
+    grid: {
+      top: '20%',
+      left: '3%',
+      right: '4%',
+      bottom: '3%',
+      show: true,// 显示边框
+      borderColor: '#012f4a',// 边框颜色
+      containLabel: true // 包含刻度文字在内
+    },
+    xAxis: {
+        type: 'category',
+        data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+        axisTick: {
+          show: false
+        },
+        axisLabel: {
+          color: '#4c9bfd' // 文本颜色
+        },
+        axisLine: {
+          show: false // 去除轴线
+        },
+        boundaryGap: false  // 去除轴内间距
+    },
+    yAxis: {
+        type: 'value',
+        axisTick: {
+          show: false
+        },
+        axisLabel: {
+          color: '#4c9bfd' // 文本颜色
+        },
+        axisLine: {
+          show: false // 去除轴线
+        },
+        splitLine: {
+          lineStyle: {
+            color: '#012f4a' // 分割线颜色
+          }
+        },
+        boundaryGap: false  // 去除轴内间距
+    },
+    series: [
+        {
+            name: '新增粉丝',
+            type: 'line',
+            stack: '总量',
+            data:  yearData[0].data[0],
+            // 折线修饰为圆滑
+            smooth: true,
+        },
+        {
+            name: '新增游客',
+            type: 'line',
+            stack: '总量',
+            data: yearData[0].data[1],
+            // 折线修饰为圆滑
+            smooth: true,
+        }
+    ]
+};
+
+  myChart.setOption(option);
+  window.addEventListener('resize', function () {
+    myChart.resize()
+  })
+  // 点解切换
+  $(".line1 h2").on("click", "a", function() {
+    var obj = yearData[$(this).index()]
+    option.series[0].data = obj.data[0]
+    option.series[1].data = obj.data[1]
+    // 需要重新渲染图标
+    myChart.setOption(option);
+  })
+})();
+// 折线图2
+(function() {
+  var myChart = echarts.init(document.querySelector(".line2 .chart"));
+  myChart.setOption(option);
+  window.addEventListener('resize', function () {
+    myChart.resize()
+  })
+})();
+// 饼状图1
+(function() {
+  var myChart = echarts.init(document.querySelector(".line1 .chart"));
+  myChart.setOption(option);
+  window.addEventListener('resize', function () {
+    myChart.resize()
+  })
+})();
+// 饼状图2
+(function() {
+  var myChart = echarts.init(document.querySelector(".line1 .chart"));
+  myChart.setOption(option);
+  window.addEventListener('resize', function () {
+    myChart.resize()
+  })
+})();
+// 地图
+(function() {
+  var myChart = echarts.init(document.querySelector(".line1 .chart"));
+  myChart.setOption(option);
+  window.addEventListener('resize', function () {
+    myChart.resize()
+  })
+})();
